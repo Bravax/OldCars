9e77660c84033686ff2a3d297e3f28dcf1d7a503
bravax <xavierlobez@hotmail.fr>
2024-09-26 22:12:45 +0200

    Creating the App.jsx component and App.scss Modification of the access road

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <h1>Hello World </h1>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
